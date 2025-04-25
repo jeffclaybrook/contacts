3148905c139053d72e492816c2ad8ec2ddfdb98e
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2025-04-25 10:48:09 -0500

    Initial commit

--- src/lib/update-state.ts
+++
@@ -1,9 +0,0 @@
-export function updateState(address: string, states: { name: string; abbr: string }[]): string {
- let updatedAddress = address
- states.forEach(state => {
-  const regex = new RegExp(`\\b${state.name}\\b`, "g")
-  updatedAddress = updatedAddress.replace(regex, state.abbr)
- })
-
- return updatedAddress
-}
